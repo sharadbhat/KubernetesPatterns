700a4adafd81c08bac7cd25a20ce00f94be4754a
sharadbhat <sharad.mbhat@gmail.com>
2020-10-02 21:42:44 +0530

    main

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	periodicjob "github.com/sharadbhat/KubernetesPatterns/BehavioralPatterns/PeriodicJob"
 	sidecar "github.com/sharadbhat/KubernetesPatterns/StructuralPatterns/Sidecar"
 )
 
 func main() {
+	// Behavioral Patterns
+	periodicjob.GeneratePeriodicJobSpec()
+
+	// Structural Patterns
 	sidecar.GenerateSidecarSpec()
 }
