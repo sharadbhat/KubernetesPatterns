d11030e1824b492c0c0acf25398d60cd5b63198f
sharadbhat <sharad.mbhat@gmail.com>
2020-10-02 21:42:36 +0530

    cronjob

---
+++ BehavioralPatterns/PeriodicJob/README.md
@@ -0,0 +1,9 @@
+# Periodic Job
+
+Extends Batch Job by adding a time dimension.
+
+## Problem
+
+To automate system maintenance, administrative tasks or certain specific periodic tasks, we need to use specialized scheduling software or Cron.
+
+A simple service that has to do a certain task daily may end up requiring multiple nodes, a leader, etc. Kubernetes solves this using CronJob.
---
+++ BehavioralPatterns/PeriodicJob/periodicJob.go
@@ -0,0 +1,55 @@
+package periodicjob
+
+import (
+	"fmt"
+
+	batchv1 "k8s.io/api/batch/v1"
+	batchv1beta1 "k8s.io/api/batch/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// GeneratePeriodicJobSpec ...
+func GeneratePeriodicJobSpec() {
+
+	successfulJobHistoryLimit := int32(3)
+	failedJobHistoryLimit := int32(3)
+	startingDeadlineSeconds := int64(60)
+	concurrenyPolicy := batchv1beta1.ConcurrencyPolicy("Forbid")
+
+	cronJob := &batchv1beta1.CronJob{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "periodic-job",
+		},
+		Spec: batchv1beta1.CronJobSpec{
+			Schedule:                   "*/1 * * * *",
+			SuccessfulJobsHistoryLimit: &successfulJobHistoryLimit,
+			FailedJobsHistoryLimit:     &failedJobHistoryLimit,
+			StartingDeadlineSeconds:    &startingDeadlineSeconds,
+			ConcurrencyPolicy:          concurrenyPolicy,
+			JobTemplate: batchv1beta1.JobTemplateSpec{
+				Spec: batchv1.JobSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							HostNetwork: true,
+							Containers: []corev1.Container{
+								{
+									Name:  "periodic-container",
+									Image: "busybox",
+									Args: []string{
+										"/bin/sh",
+										"-c",
+										"echo \"Executed at $(date)\"",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	fmt.Printf("%+v", cronJob)
+	// return cronJob
+}
