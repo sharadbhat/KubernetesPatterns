c3a4172c91c58715da8c9037a1f2d2b6e4af3ad7
sharadbhat <sharad.mbhat@gmail.com>
2020-10-04 17:04:13 +0530

    sidecar sync yaml

--- StructuralPatterns/Sidecar/sidecar.go
+++ StructuralPatterns/Sidecar/sidecar.go
@@ -9,47 +9,47 @@ import (
 // GenerateSidecarSpec ...
 func GenerateSidecarSpec() {
 	pod := &corev1.PodSpec{
-		Volumes: []corev1.Volume{
-			{
-				Name: "sharedlog",
-			},
-		},
 		Containers: []corev1.Container{
 			{
 				Name:  "main-container",
 				Image: "busybox",
-				VolumeMounts: []corev1.VolumeMount{
-					{
-						Name:      "sharedlog",
-						MountPath: "/var/log",
-					},
-				},
 				Args: []string{
 					"/bin/sh",
 					"-c",
 					`i=0;
 					while true;
 					do
-						echo "Logging line: $i" >> /var/log/file.log;
-						i=$((i+1));
-						sleep 1;
+					echo "Logging line: $i" >> /var/log/file.log;
+					i=$((i+1));
+					sleep 1;
 					done`,
 				},
-			},
-			{
-				Name:  "sidecar-container",
-				Image: "busybox",
 				VolumeMounts: []corev1.VolumeMount{
 					{
 						Name:      "sharedlog",
 						MountPath: "/var/log",
 					},
 				},
+			},
+			{
+				Name:  "sidecar-container",
+				Image: "busybox",
 				Args: []string{
 					"/bin/sh",
 					"-c",
 					"tail -f /var/log/file.log",
 				},
+				VolumeMounts: []corev1.VolumeMount{
+					{
+						Name:      "sharedlog",
+						MountPath: "/var/log",
+					},
+				},
+			},
+		},
+		Volumes: []corev1.Volume{
+			{
+				Name: "sharedlog",
 			},
 		},
 	}
