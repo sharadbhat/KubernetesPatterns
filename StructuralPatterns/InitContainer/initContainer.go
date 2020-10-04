8a851fa2cc730a34be0a8d34dadd4ec9c0f5fd12
sharadbhat <sharad.mbhat@gmail.com>
2020-10-04 17:03:58 +0530

    init fix

--- StructuralPatterns/InitContainer/initContainer.go
+++ StructuralPatterns/InitContainer/initContainer.go
@@ -9,41 +9,43 @@ import (
 // GenerateInitContainerSpec ...
 func GenerateInitContainerSpec() {
 	pod := &corev1.PodSpec{
-		Volumes: []corev1.Volume{
-			{
-				Name: "sharedlog",
-			},
-		},
 		Containers: []corev1.Container{
 			{
 				Name:  "main-container",
 				Image: "busybox",
-				VolumeMounts: []corev1.VolumeMount{
-					{
-						Name:      "sharedlog",
-						MountPath: "/var/log",
-					},
-				},
-				Args: []string{
-					"/bin/sh",
-					"-c",
-					"touch /var/log/file.txt",
-				},
-			},
-			{
-				Name:  "sidecar-container",
-				Image: "busybox",
-				VolumeMounts: []corev1.VolumeMount{
-					{
-						Name:      "sharedlog",
-						MountPath: "/var/log",
-					},
-				},
 				Args: []string{
 					"/bin/sh",
 					"-c",
 					"ls /var/log",
 				},
+				VolumeMounts: []corev1.VolumeMount{
+					{
+						Name:      "sharedlog",
+						MountPath: "/var/log",
+					},
+				},
+			},
+		},
+		InitContainers: []corev1.Container{
+			{
+				Name:  "init-container",
+				Image: "busybox",
+				Args: []string{
+					"/bin/sh",
+					"-c",
+					"touch /var/log/file.txt",
+				},
+				VolumeMounts: []corev1.VolumeMount{
+					{
+						Name:      "sharedlog",
+						MountPath: "/var/log",
+					},
+				},
+			},
+		},
+		Volumes: []corev1.Volume{
+			{
+				Name: "sharedlog",
 			},
 		},
 	}
