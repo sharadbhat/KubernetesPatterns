308ee94b961b6f17ef758fabad31daa0b1e08695
sharadbhat <sharad.mbhat@gmail.com>
2020-10-05 16:58:22 +0530

    secret

---
+++ ConfigurationPatterns/ConfigurationResource/secret.go
@@ -0,0 +1,26 @@
+package configurationresource
+
+import (
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// GenerateSecretSpec ...
+func GenerateSecretSpec() {
+
+	secret := corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "secret-data",
+		},
+		Type: corev1.SecretTypeOpaque,
+		Data: map[string][]byte{
+			"username": []byte("dGVzdA=="), // "test" in base64
+			"password": []byte("MTIzNAo="), // "1234" in base64
+		},
+	}
+
+	fmt.Printf("%+v", secret)
+	// return configMap
+}
