314950a0a9b8bdef6b0d903739a9a9327df6a719
sharadbhat <sharad.mbhat@gmail.com>
2020-10-05 16:58:16 +0530

    config map

---
+++ ConfigurationPatterns/ConfigurationResource/configMap.go
@@ -0,0 +1,27 @@
+package configurationresource
+
+import (
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// GenerateConfigMapSpec ...
+func GenerateConfigMapSpec() {
+	configMap := corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "config-map-data",
+		},
+		Data: map[string]string{
+			"sample-value": "from-config",
+			"sample-data": `
+			port=8080
+			root=/
+			`,
+		},
+	}
+
+	fmt.Printf("%+v", configMap)
+	// return configMap
+}
